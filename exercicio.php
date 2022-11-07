55c024390252be3d6406f833d83e533cbb8a3604
Gustavo Henrique <gustavohcsantos@gmail.com>
2022-11-07 14:00:25 -0300

    Atualização do item do menu 'Sem formatação'.

--- exercicio.php
+++ exercicio.php
@@ -12,12 +12,15 @@
         <h2>Visualização do Exercício</h2>
     </header>
     <nav class="navegacao">
-        <a href="#" class="verde">Sem formatação</a>
+    <a href=<?= "./teste/teste.php"?> class="verde">Sem formatação</a>
         <a href="index.php" class="vermelho">Voltar</a>
     </nav>
     <main class="principal">
         <div class="conteudo">
-            
+            <?php
+                //include($_GET['dir'] . "/" . $_GET['file'] . ".php");
+                include("./teste/teste.php");
+            ?>
         </div>
     </main>
     <footer class="rodape">
